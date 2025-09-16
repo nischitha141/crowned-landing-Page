554e93a7677296160adf1b1892b17f47487139b2
Aswin-optiblack <aswin@optiblack.com>
2025-09-16 10:08:03 +0530

    fix: added lock icon on mobile screen

--- src/components/creators/BrandCrowns/index.tsx
+++ src/components/creators/BrandCrowns/index.tsx
@@ -39,7 +39,13 @@ const BrandCrowns = () => {
     },
   ];
 
-  const { scrollContainerRef, scrollLeft, scrollRight, handleMouseEnter, handleMouseLeave } = useInfiniteScroll(brandCards.length);
+  const {
+    scrollContainerRef,
+    scrollLeft,
+    scrollRight,
+    handleMouseEnter,
+    handleMouseLeave,
+  } = useInfiniteScroll(brandCards.length);
 
   return (
     <div className="mx-4 sm:mx-8 lg:mx-20 my-8 sm:my-12 lg:my-20">
@@ -47,16 +53,15 @@ const BrandCrowns = () => {
         <div className="flex gap-1 sm:gap-2 items-center">
           <h3 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bowlby text-[#424242] leading-tight">
             Crowns That Hit Different
+            {/* Show lock icon right after "Crowns" on mobile only */}
+            <Image
+              src="assets/lock.svg"
+              alt="Crowned"
+              width={50}
+              height={40}
+              className="sm:hidden w-[30px] h-[30px] ml-1 inline-block align-middle"
+            />
           </h3>
-
-          {/* Hide lock icon on mobile, show on desktop */}
-          <Image
-            src="assets/lock.svg"
-            alt="Crowned"
-            width={50}
-            height={40}
-            className="hidden sm:block sm:w-[50px] sm:h-[50px] lg:w-[70px] lg:h-[70px]"
-          />
         </div>
 
         <div className="flex gap-2 items-center justify-center">
@@ -64,8 +69,20 @@ const BrandCrowns = () => {
             onClick={scrollLeft}
             className="w-12 h-12 sm:w-14 sm:h-14 border-[2px] border-[#E8E0EF] bg-[#FAFAFA] hover:bg-[#d0d0d0] transition-colors rounded-full cursor-pointer flex items-center justify-center"
           >
-            <svg width="16" height="14" viewBox="0 0 20 17" fill="none" xmlns="http://www.w3.org/2000/svg" className="sm:w-[20px] sm:h-[17px]">
-              <path fillRule="evenodd" clipRule="evenodd" d="M9.35338 0.79696C8.90261 0.35798 8.17177 0.35798 7.721 0.79696L0.795379 7.54143C0.344608 7.98041 0.344608 8.69213 0.795378 9.13111L7.721 15.8756C8.17177 16.3146 8.90261 16.3146 9.35338 15.8756C9.80415 15.4366 9.80415 14.7249 9.35338 14.2859L4.39823 9.46035H22.3884C23.0259 9.46035 23.5427 8.95708 23.5427 8.33627C23.5427 7.71546 23.0259 7.21219 22.3884 7.21219L4.39823 7.21219L9.35338 2.38665C9.80415 1.94767 9.80415 1.23594 9.35338 0.79696Z" fill="#9E9E9E"/>
+            <svg
+              width="16"
+              height="14"
+              viewBox="0 0 20 17"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="sm:w-[20px] sm:h-[17px]"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M9.35338 0.79696C8.90261 0.35798 8.17177 0.35798 7.721 0.79696L0.795379 7.54143C0.344608 7.98041 0.344608 8.69213 0.795378 9.13111L7.721 15.8756C8.17177 16.3146 8.90261 16.3146 9.35338 15.8756C9.80415 15.4366 9.80415 14.7249 9.35338 14.2859L4.39823 9.46035H22.3884C23.0259 9.46035 23.5427 8.95708 23.5427 8.33627C23.5427 7.71546 23.0259 7.21219 22.3884 7.21219L4.39823 7.21219L9.35338 2.38665C9.80415 1.94767 9.80415 1.23594 9.35338 0.79696Z"
+                fill="#9E9E9E"
+              />
             </svg>
           </button>
 
@@ -73,8 +90,20 @@ const BrandCrowns = () => {
             onClick={scrollRight}
             className="w-12 h-12 sm:w-14 sm:h-14 bg-[#e7e7e7] hover:bg-[#c8c8ca] transition-colors rounded-full cursor-pointer flex items-center justify-center"
           >
-            <svg width="16" height="14" viewBox="0 0 24 17" fill="none" xmlns="http://www.w3.org/2000/svg" className="sm:w-[20px] sm:h-[17px]">
-              <path fillRule="evenodd" clipRule="evenodd" d="M14.9591 0.633874C15.4099 0.194894 16.1407 0.194894 16.5915 0.633874L23.5171 7.37834C23.9679 7.81732 23.9679 8.52905 23.5171 8.96803L16.5915 15.7125C16.1407 16.1515 15.4099 16.1515 14.9591 15.7125C14.5083 15.2735 14.5083 14.5618 14.9591 14.1228L19.9143 9.29726H1.92407C1.28658 9.29726 0.7698 8.794 0.769801 8.17318C0.769801 7.55237 1.28659 7.04911 1.92407 7.04911L19.9143 7.04911L14.9591 2.22356C14.5083 1.78458 14.5083 1.07285 14.9591 0.633874Z" fill="#583A73"/>
+            <svg
+              width="16"
+              height="14"
+              viewBox="0 0 24 17"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="sm:w-[20px] sm:h-[17px]"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M14.9591 0.633874C15.4099 0.194894 16.1407 0.194894 16.5915 0.633874L23.5171 7.37834C23.9679 7.81732 23.9679 8.52905 23.5171 8.96803L16.5915 15.7125C16.1407 16.1515 15.4099 16.1515 14.9591 15.7125C14.5083 15.2735 14.5083 14.5618 14.9591 14.1228L19.9143 9.29726H1.92407C1.28658 9.29726 0.7698 8.794 0.769801 8.17318C0.769801 7.55237 1.28659 7.04911 1.92407 7.04911L19.9143 7.04911L14.9591 2.22356C14.5083 1.78458 14.5083 1.07285 14.9591 0.633874Z"
+                fill="#583A73"
+              />
             </svg>
           </button>
         </div>
